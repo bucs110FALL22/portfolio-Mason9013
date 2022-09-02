da4fff22cc8f83f6c98ac02cb40194f861589213
Mnash1 <112529351+Mason9013@users.noreply.github.com>
2022-09-02 15:05:29 +0000

    Exchange rate function

---
+++ ch01/exercises/simple.py
@@ -0,0 +1,17 @@
+print(10 * 5)
+print(10 ** 2)
+print(15 / 10)
+print(15 // 10)
+print(-15 // 10)
+print(15 % 10)
+print(10 % 15)
+print(10 % 10)
+print(0 % 10)
+print(10 / 15)
+# it didn't round right
+
+rate = int(input("What is the current exchange rate of Euros to USD?"))
+amount = int(input("How much do you want to exchange?"))
+total = amount/rate
+result = total - 3
+print(result)
