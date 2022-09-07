cf8dea092d1d9005b4509ae21df0536ad103c6d6
Mnash1 <112529351+Mason9013@users.noreply.github.com>
2022-09-07 22:07:03 +0000

    two squares

---
+++ ch02/exercises/myturtle.py
@@ -0,0 +1,29 @@
+import turtle
+dog = turtle.Turtle()
+
+dog.shape("turtle")
+dog.color("purple")
+
+dog.forward(50)
+dog.right(90)
+dog.forward(50)
+dog.right(90)
+dog.forward(50)
+dog.right(90)
+dog.forward(50)
+
+dog.color("red")
+dog.up()
+dog.forward(20)
+dog.down()
+
+dog.forward(50)
+dog.right(90)
+dog.forward(50)
+dog.right(90)
+dog.forward(50)
+dog.right(90)
+dog.forward(50)
+
+window = turtle.Screen()
+window.exitonclick()
--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,10 +1,25 @@
-
+import random
 #Part A
 weeks = 16
+print(weeks,type(weeks))
+
 classes = 5
+print(classes,type(classes))
+
 tuition = 6000
+print(tuition,type(tuition))
+
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
+print(cost_per_week,type(cost_per_week))
 
+classes_per_week = 3
+print(classes_per_week,type(classes_per_week))
+
+cost_per_class = (cost_per_week/classes_per_week)
+print("Hello user how are you? Cost per class:", cost_per_class)
 
 #Part B
+random_numbers = [29, 47, 79, 10, 56]
+random_number = random.choice(random_numbers)
+print("Your random number is:", random_number)
