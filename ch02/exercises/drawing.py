1bed1d930f0e681dedb211a487e9d5a0f74bec71
Mnash1 <112529351+Mason9013@users.noreply.github.com>
2022-09-09 20:49:32 +0000

    User side choice turtle

--- ch02/exercises/drawing.py
+++ ch02/exercises/drawing.py
@@ -1,7 +1,16 @@
 import turtle
 
-sides = input(print("How many sides do you want?"))
-length = input(print("How long do you want each side to be?"))
+sides = int(input("How many sides do you want:"))
+length = int(input("How long do you want each side to be:"))
 
 cat = turtle.Turtle()
 cat.color("red")
+
+angle = 360/sides
+
+for i in [angle]*sides:
+  cat.forward(length)
+  cat.left(i)
+
+window = turtle.Screen()
+window.exitonclick()
