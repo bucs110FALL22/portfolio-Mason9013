a1fed239b031c676f4204dd07e22867e5c970eda
Mnash1 <112529351+Mason9013@users.noreply.github.com>
2022-10-05 01:05:04 +0000

    Completed exercise coinflip

--- ch04/exercises/coinflip.py
+++ ch04/exercises/coinflip.py
@@ -8,17 +8,21 @@ bird = turtle.Turtle()
 pos = turtle.pos()
 screen = window.screensize()
 
+print(window.screensize()) #(400,300)
 
-
-while pos <= screen:
+while bird.xcor() <= 200 and bird.xcor() >= -200 and bird.ycor() <= 150 and bird.ycor() >= -150:
   number = random.randrange(0,2)
   if number != 0:
     bird.left(90)
     bird.forward(50)
+    bird.xcor()
+    bird.ycor()
   elif number == 0:
     bird.right(90)
     bird.forward(50)
-  pos = turtle.pos()
-
+    bird.xcor()
+    bird.ycor()
+    
 window.exitonclick()
+  
   
