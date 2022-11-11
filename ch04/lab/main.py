4a73b808b4f07da594d8298a66dd69540dc76a49
Mnash1 <112529351+Mason9013@users.noreply.github.com>
2022-11-11 17:28:16 +0000

    initial commit ch08 lab

--- ch04/lab/main.py
+++ ch04/lab/main.py
@@ -1,7 +1,12 @@
 import pygame 
 pygame.init()
-window = pygame.display.setmode()
-window.exitonclick()
 
-pygame.draw.circle
+window = pygame.display.set_mode((400,400))
+window.fill("blue")
+surface = pygame.display.get_surface()
+
+print(pygame.display.get_window_size())
+
+pygame.draw.circle(surface, "red", (200,200), 100)
+pygame.display.flip
 
--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -1,1 +1,26 @@
-print("hello")
+n = 101
+count = 0
+while n != 1:
+  if n % 2 == 0:
+    n = n/2
+  elif n % 2 != 0:
+    n = 3*n+1
+  count += 1
+  #print(n)
+print(count)
+
+upper_limit = 20
+iters = {}
+
+countt = 0
+start = range(2,upper_limit+1)
+
+for n in start:
+  if n % 2 == 0:
+    n = n/2
+  elif n % 2 != 0:
+    n = 3*n+1
+  countt += 1
+  iters['count'] = 'n'
+
+print(iters)
